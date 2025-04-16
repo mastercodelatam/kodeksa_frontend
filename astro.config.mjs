a8e105fbc418d62fb6b209d3ef8f43fb47f6b57f
mastercodelatam <79774226+mastercodelatam@users.noreply.github.com>
2025-04-15 21:16:53 -0500

    site map , menus

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,15 +1,54 @@
 import { defineConfig } from 'astro/config';
-import tailwind from '@astrojs/tailwind';
-import react from '@astrojs/react';
 import node from '@astrojs/node';
+import react from '@astrojs/react';
+import sitemap from '@astrojs/sitemap';
+import tailwind from '@astrojs/tailwind';
 
 export default defineConfig({
-  output: 'server',  // Cambiado de 'static' a 'server'
+  output: 'server',
   adapter: node({
-    mode: 'standalone'  // Modo para ejecutar como servidor independiente
+    mode: 'standalone'
   }),
   integrations: [
-    tailwind(),
-    react()
-  ]
+    tailwind({
+      // applyBaseStyles: false
+    }),
+    react(),
+    sitemap({
+      lastmod: new Date(),
+      changefreq: 'daily',
+      priority: 0.7,
+      filter: (page) => !page.url.includes('/secret/'),
+      defaultLocale: 'es',
+      entryLimit: 50000,
+      createLinkInHead: true,
+      i18n: {
+        defaultLocale: 'en',
+        locales: {
+          en: 'en',
+          es: 'es'
+        }
+      }
+    })
+  ],
+  site: 'https://kodeksa.lat',
+  build: {
+    minify: true,
+    cssMinify: true,
+    jsMinify: true
+  },
+  vite: {
+    build: {
+      chunkSizeWarningLimit: 1000,
+      rollupOptions: {
+        output: {
+          manualChunks: (id) => {
+            if (id.includes('node_modules')) {
+              return 'vendor';
+            }
+          }
+        }
+      }
+    }
+  }
 });
